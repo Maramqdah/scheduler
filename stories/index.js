bb35f3938790023775102daeb439a50ab8eb2298
Maram Qdah <maram.m.qdah@gmail.com>
2021-10-27 21:02:27 -0400

    added more aponitment views

--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,11 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+
 
 const days = [
   {
@@ -148,7 +153,15 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header",()=><Header time="12pm"/>)
-  .add("Empty", () => <Empty onAdd={action("onAdd")}/>);
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show",()=><Show student="Lydia Miller-Jones" interviewer={interviewer.name}
+  onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
+  .add("Confirm",()=><Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} 
+  onCancel={action("onCancel")}  />)
+  .add("Status",()=><Status message="Deleteing"/>)
+  .add("Status",()=><Status message="Could not delete appointment." onClose={action("onClose")}/>)
+  .add("Error",()=><Error message="Could not delete appointment" onClose={action("onClose")}/>);
+
  
 
   
